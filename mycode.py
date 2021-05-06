621eac872f6cd8fdd5f2c94db6cbd6028027c899
root <root@LDCC-VN.localdomain>
2021-05-06 11:45:58 +0700

    first time commit

---
+++ mycode.py
@@ -0,0 +1,8 @@
+print("Hello, guys now we create a blog to help the new learners")
+i=1
+while(True):
+    if(i<10):
+        print(i)
+    break
+print("Finally, I created a small code for run a Jenkins jobs")
+print("GOOD BYE !!!")
